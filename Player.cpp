6f4dc02ad9a9d9e9dfc6e298ba296fd43f89a1c6
parashchuk6574 <parashchuk,anton@chnu.edu.ua>
2024-04-02 11:39:23 +0300

    Laba-3 class Player

--- Player.cpp
+++ Player.cpp
@@ -1,6 +1,23 @@
 #include "Player.h"
 std::string Cars;
-Player::Player() {}
+
+int Player::playerCount = 0;
+
+Player::Player() {
+    playerCount++;
+}
+
+Player::Player(const Player &other) {
+    playerCount++;
+    this->car = other.car;
+    this->numberCar = other.numberCar;
+}
+
+Player::Player(Player &&other) noexcept {
+    playerCount++;
+    this->car = std::move(other.car);
+    this->numberCar = std::move(other.numberCar);
+}
 
 void Player::CreateCar() {
     std::cout << "Please enter the name of the car:";
@@ -19,12 +36,41 @@ void Player::CarPanel() {
               << "Number: " << numberCar << std::endl;
 }
 
-
 void Player::ParkingCars() {
     Cars = car;
 }
 
-std::string Player::getCar(){
-    ParkingCars();
-    return Cars;
+std::string Player::getCar() {
+    return std::move(this->car);
+}
+
+Player& Player::operator++() {
+    playerCount++;
+    return *this;
+}
+
+Player Player::operator++(int) {
+    Player temp(*this);
+    ++(*this);
+    return temp;
+}
+
+Player Player::operator+(const Player& other) const {
+    Player result;
+    result.car = car + other.car;
+    result.numberCar = numberCar + other.numberCar;
+    return result;
+}
+
+std::istream& operator>>(std::istream& in, Player& player) {
+    in >> player.car >> player.numberCar;
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const Player& player) {
+    return out;
+}
+
+int Player::getPlayerCount() {
+    return playerCount;
 }
--- Player.h
+++ Player.h
@@ -3,19 +3,29 @@
 
 #include <iostream>
 #include <string>
-#include <vector>
 
 class Player {
 private:
-    std::string car = "No Car!";
-    std::string numberCar = "No Number Car!";
+    std::string car;
+    std::string numberCar;
+    static int playerCount;
+
 public:
     Player();
+    Player(const Player& other);
+    Player(Player&& other) noexcept;
     void CreateCar();
     void CreateNumberCar();
     void CarPanel();
     void ParkingCars();
     std::string getCar();
+    static int getPlayerCount();
+
+    Player& operator++();
+    Player operator++(int);
+    Player operator+(const Player& other) const;
+    friend std::istream& operator>>(std::istream& in, Player& player);
+    friend std::ostream& operator<<(std::ostream& out, const Player& player);
 };
 
 #endif // PLAYER_H

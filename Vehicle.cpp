beaf64c4cfc2fb50cf8f4884da4d95f60069c6be
parashchuk6574 <parashchuk,anton@chnu.edu.ua>
2024-04-25 20:03:10 +0300

    add new class Vehicle laba-5

--- Vehicle.cpp
+++ Vehicle.cpp
@@ -1,5 +1,52 @@
-//
-// Created by Acer on 25.04.2024.
-//
-
 #include "Vehicle.h"
+using namespace std;
+// Ініціалізація статичного члена
+int Vehicle::playerCount = 0;
+
+// Конструктори
+Vehicle::Vehicle() : type("Undefined"), brand("Undefined"), model("Undefined"), year(0) {
+    playerCount++;
+}
+
+Vehicle::Vehicle(const string& type, const string& brand, const string& model, int year)
+        : type(type), brand(brand), model(model), year(year) {
+    playerCount++;
+}
+
+Vehicle::Vehicle(const Vehicle& other)
+        : type(other.type), brand(other.brand), model(other.model), year(other.year) {
+    playerCount++;
+}
+
+Vehicle::Vehicle(Vehicle&& other) noexcept
+        : type(move(other.type)), brand(move(other.brand)), model(move(other.model)), year(other.year) {
+    playerCount++;
+    other.year = 0;
+}
+
+// Оператори
+Vehicle& Vehicle::operator++() {
+    year++;
+    return *this;
+}
+
+
+// Друзі класу
+istream& operator>>(istream& in, Vehicle& vehicle) {
+    in >> vehicle.type >> vehicle.brand >> vehicle.model >> vehicle.year;
+    return in;
+}
+
+ostream& operator<<(ostream& out, const Vehicle& vehicle) {
+    out << "Type: " << vehicle.type << ", Brand: " << vehicle.brand << ", Model: " << vehicle.model << ", Year: " << vehicle.year;
+    return out;
+}
+
+// Віртуальний деструктор
+Vehicle::~Vehicle() {
+    playerCount--;
+}
+
+void Vehicle::display() const {
+    cout << "Type: " << type << ", Brand: " << brand << ", Model: " << model << ", Year: " << year << endl;
+}
--- Vehicle.h
+++ Vehicle.h
@@ -1,14 +1,34 @@
-//
-// Created by Acer on 25.04.2024.
-//
+#ifndef VEHICLE_H
+#define VEHICLE_H
 
-#ifndef LABA3_VEHICLE_H
-#define LABA3_VEHICLE_H
-
-
+#include <string>
+#include <iostream>
+using namespace std;
 class Vehicle {
+protected:
+    string type;
+    string brand;
+    string model;
+    int year;
+    static int playerCount;
 
+public:
+    Vehicle();
+    Vehicle(const string& type, const string& brand, const string& model, int year);
+    Vehicle(const Vehicle& other);
+    Vehicle(Vehicle&& other) noexcept;
+
+    static int getPlayerCount() { return playerCount; }
+
+    Vehicle& operator++();
+
+    friend istream& operator>>(istream& in, Vehicle& vehicle);
+    friend ostream& operator<<(ostream& out, const Vehicle& vehicle);
+
+    virtual string getTypeInEnglish() const = 0;
+    virtual void showAdditionalInfo() const = 0;
+    virtual ~Vehicle();
+    virtual void display() const;
 };
 
-
-#endif //LABA3_VEHICLE_H
+#endif // VEHICLE_H
